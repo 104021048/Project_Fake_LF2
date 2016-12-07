2e7adc9784c5064d8ad3c12d3df8b18eea597bc6
Jonhson1997 <johnson97312@gmail.com>
2016-12-07 15:25:16 +0800

    client V5

--- src/application/Client.java
+++ src/application/Client.java
@@ -11,29 +11,37 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Client extends Application {
 	private ClientCenter clientCenter;
-	public Scene scene_login , scene_room;
+	public Scene scene_login, scene_room, scene_game;
 	public Socket sock;
-	public BufferedReader reader;
-	public PrintStream writer;
-	public GridPane gridpane_login_root , gridpane_room_root;
-	public TextField textfield_login_ip , textfield_login_name;
-	public Label label_login_ip , label_login_name;
-	public Button button_login_connect , button_login_exit , button_room_character1 , button_room_character2 , button_room_character3 , button_room_character4 , 
-	button_room_character5 , button_room_ready;
-	public ImageView image_login_logo , image_room_player_1 , image_room_player_2 , image_room_player_3 , image_room_player_4;
-	public Label label_room_headpicture1 , label_room_headpicture2 , label_room_headpicture3 , label_room_headpicture4 ,
-	label_room_name1 , label_room_name2 , label_room_name3 , label_room_name4 , label_room_systemmessage;
+	public GridPane gridpane_login_root, gridpane_room_root;
+	public StackPane stackpane_game_root, stackpane_game_backgroundblood, stackpane_game_backgroundsky,
+			stackpane_game_backgroundground;
+	public TextField textfield_login_ip, textfield_login_name;
+	public Label label_login_ip, label_login_name, label_room_headpicture1, label_room_headpicture2,
+			label_room_headpicture3, label_room_headpicture4, label_room_name1, label_room_name2, label_room_name3,
+			label_room_name4, label_room_systemmessage, label_game_backgroundblood, label_game_backgroundsky,
+			label_game_backgroundground, label_game_headpicture, label_game_character1, label_game_character2,
+			label_game_character3, label_game_character4;
+	public Button button_login_connect, button_login_exit, button_room_character1, button_room_character2,
+			button_room_character3, button_room_character4, button_room_character5, button_room_ready;
+	public ImageView image_login_logo, image_room_player_1, image_room_player_2, image_room_player_3,
+			image_room_player_4;
 	public HBox hbox_room_button;
 	public String name;
-	public void login_init(){
+	public ProgressBar progressbar_game_blood, progressbar_game_characterblood1, progressbar_game_characterblood2,
+			progressbar_game_characterblood3, progressbar_game_characterblood4;
+
+	public void login_init() {
 		gridpane_login_root = new GridPane();
 		gridpane_login_root.setPadding(new Insets(25, 25, 25, 25));
 		image_login_logo = new ImageView("logo.png");
@@ -44,14 +52,14 @@ public class Client extends Application {
 		button_login_connect = new Button("Connect");
 		button_login_exit = new Button("Exit");
 	}
-	
+
 	public void login_setupUI() {
-		label_login_ip.setPrefSize(50,10);
+		label_login_ip.setPrefSize(50, 10);
 		textfield_login_ip.setPrefSize(150, 10);
-		label_login_name.setPrefSize(50,10);
-		textfield_login_name.setPrefSize(150,10);
-		button_login_connect.setPrefSize(100,10);
-		button_login_exit.setPrefSize(100,10);
+		label_login_name.setPrefSize(50, 10);
+		textfield_login_name.setPrefSize(150, 10);
+		button_login_connect.setPrefSize(100, 10);
+		button_login_exit.setPrefSize(100, 10);
 		gridpane_login_root.setHgap(10);
 		gridpane_login_root.setVgap(10);
 		gridpane_login_root.add(image_login_logo, 1, 1, 10, 1);
@@ -69,7 +77,7 @@ public class Client extends Application {
 		button_login_connect.setOnAction(e -> {
 			name = textfield_login_name.getText().toString();
 			String ip = textfield_login_ip.getText().toString();
-			Thread t = new Thread(clientCenter = new ClientCenter(primaryStage,sock,ip,name));
+			Thread t = new Thread(clientCenter = new ClientCenter(primaryStage, sock, ip, name));
 			t.start();
 			room_init();
 			room_setupUI();
@@ -81,13 +89,13 @@ public class Client extends Application {
 		});
 	}
 
-	public void room_init(){
+	public void room_init() {
 		image_room_player_1 = new ImageView("role_1.png");
 		image_room_player_2 = new ImageView("role_1.png");
 		image_room_player_3 = new ImageView("role_1.png");
 		image_room_player_4 = new ImageView("role_1.png");
 		label_room_headpicture1 = new Label();
-		label_room_headpicture2 = new Label(); 
+		label_room_headpicture2 = new Label();
 		label_room_headpicture3 = new Label();
 		label_room_headpicture4 = new Label();
 		label_room_name1 = new Label(name);
@@ -105,7 +113,7 @@ public class Client extends Application {
 		gridpane_room_root = new GridPane();
 		gridpane_room_root.setPadding(new Insets(25, 25, 25, 25));
 	}
-	
+
 	public void room_setupUI() {
 		gridpane_room_root.setHgap(10);
 		gridpane_room_root.setVgap(10);
@@ -113,28 +121,28 @@ public class Client extends Application {
 		label_room_headpicture2.setGraphic(image_room_player_2);
 		label_room_headpicture3.setGraphic(image_room_player_3);
 		label_room_headpicture4.setGraphic(image_room_player_4);
-		label_room_headpicture1.setPrefSize(150,100);
-		label_room_headpicture2.setPrefSize(150,100);
-		label_room_headpicture3.setPrefSize(150,100);
-		label_room_headpicture4.setPrefSize(150,100);
-		label_room_name1.setPrefSize(150,30);
-		label_room_name2.setPrefSize(150,30);
-		label_room_name3.setPrefSize(150,30);
-		label_room_name4.setPrefSize(150,30);
-		button_room_character1.setPrefSize(50,50);
-		button_room_character2.setPrefSize(50,50);
-		button_room_character3.setPrefSize(50,50);
-		button_room_character4.setPrefSize(50,50);
-		button_room_character5.setPrefSize(50,50);
-		button_room_ready.setPrefSize(100,30);
-		label_room_systemmessage.setPrefSize(300,30);
+		label_room_headpicture1.setPrefSize(150, 100);
+		label_room_headpicture2.setPrefSize(150, 100);
+		label_room_headpicture3.setPrefSize(150, 100);
+		label_room_headpicture4.setPrefSize(150, 100);
+		label_room_name1.setPrefSize(150, 30);
+		label_room_name2.setPrefSize(150, 30);
+		label_room_name3.setPrefSize(150, 30);
+		label_room_name4.setPrefSize(150, 30);
+		button_room_character1.setPrefSize(50, 50);
+		button_room_character2.setPrefSize(50, 50);
+		button_room_character3.setPrefSize(50, 50);
+		button_room_character4.setPrefSize(50, 50);
+		button_room_character5.setPrefSize(50, 50);
+		button_room_ready.setPrefSize(100, 30);
+		label_room_systemmessage.setPrefSize(300, 30);
 
 		button_room_character1.setGraphic(new ImageView("button_role_1.png"));
 		button_room_character2.setGraphic(new ImageView("button_role_2.png"));
 		button_room_character3.setGraphic(new ImageView("button_role_3.png"));
 		button_room_character4.setGraphic(new ImageView("button_role_4.png"));
 		button_room_character5.setGraphic(new ImageView("button_role_5.png"));
-		
+
 		hbox_room_button.setPadding(new Insets(25, 25, 25, 25));
 		hbox_room_button.setSpacing(10);
 		hbox_room_button.getChildren().add(button_room_character1);
@@ -143,52 +151,166 @@ public class Client extends Application {
 		hbox_room_button.getChildren().add(button_room_character4);
 		hbox_room_button.getChildren().add(button_room_character5);
 
-		gridpane_room_root.add(label_room_headpicture1, 1, 1,1,1);
-		gridpane_room_root.add(label_room_headpicture2, 3, 1,1,1);
-		gridpane_room_root.add(label_room_headpicture3, 5, 1,1,1);
-		gridpane_room_root.add(label_room_headpicture4, 7, 1,1,1);
-		gridpane_room_root.add(label_room_name1, 1, 2,1,1);
-		gridpane_room_root.add(label_room_name2, 3, 2,1,1);
-		gridpane_room_root.add(label_room_name3, 5, 2,1,1);
-		gridpane_room_root.add(label_room_name4, 7, 2,1,1);
+		gridpane_room_root.add(label_room_headpicture1, 1, 1, 1, 1);
+		gridpane_room_root.add(label_room_headpicture2, 3, 1, 1, 1);
+		gridpane_room_root.add(label_room_headpicture3, 5, 1, 1, 1);
+		gridpane_room_root.add(label_room_headpicture4, 7, 1, 1, 1);
+		gridpane_room_root.add(label_room_name1, 1, 2, 1, 1);
+		gridpane_room_root.add(label_room_name2, 3, 2, 1, 1);
+		gridpane_room_root.add(label_room_name3, 5, 2, 1, 1);
+		gridpane_room_root.add(label_room_name4, 7, 2, 1, 1);
 		gridpane_room_root.add(hbox_room_button, 3, 5, 8, 1);
 		gridpane_room_root.add(button_room_ready, 1, 6, 3, 1);
-		gridpane_room_root.add(label_room_systemmessage, 4,6,6,1);
-		
-		scene_room = new Scene(gridpane_room_root , 800 , 600);
+		gridpane_room_root.add(label_room_systemmessage, 4, 6, 6, 1);
+
+		scene_room = new Scene(gridpane_room_root, 800, 600);
 		scene_room.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
 	}
 
 	public void room_setupListener(Stage primaryStage) {
-		 button_room_character1.setOnAction(e -> {
-			 image_room_player_1 = new ImageView("role_1.png");
-			 label_room_headpicture1.setGraphic(image_room_player_1);
-			clientCenter.SelectedRole_1();
-		 });
-		 button_room_character2.setOnAction(e -> {
-			 image_room_player_1 = new ImageView("role_2.png");
-			 label_room_headpicture1.setGraphic(image_room_player_1);
-			 clientCenter.SelectedRole_2();
-		 });
-		 button_room_character3.setOnAction(e -> {
-			 image_room_player_1 = new ImageView("role_3.png");
-			 label_room_headpicture1.setGraphic(image_room_player_1);
-			 clientCenter.SelectedRole_3();
-		 });
-		 button_room_character4.setOnAction(e -> {
-			 image_room_player_1 = new ImageView("role_4.png");
-			 label_room_headpicture1.setGraphic(image_room_player_1);
-			 clientCenter.SelectedRole_4();
-		 });
-		 button_room_character5.setOnAction(e -> {
-			 image_room_player_1 = new ImageView("role_5.png");
-			 label_room_headpicture1.setGraphic(image_room_player_1);
-			 clientCenter.SelectedRole_5();
-		 });
-		 button_room_ready.setOnAction(e -> {});
+		button_room_character1.setOnAction(e -> {
+			image_room_player_1 = new ImageView("role_1.png");
+			label_room_headpicture1.setGraphic(image_room_player_1);
+			clientCenter.SelectedRole(1);
+		});
+		button_room_character2.setOnAction(e -> {
+			image_room_player_1 = new ImageView("role_2.png");
+			label_room_headpicture1.setGraphic(image_room_player_1);
+			clientCenter.SelectedRole(2);
+		});
+		button_room_character3.setOnAction(e -> {
+			image_room_player_1 = new ImageView("role_3.png");
+			label_room_headpicture1.setGraphic(image_room_player_1);
+			clientCenter.SelectedRole(3);
+		});
+		button_room_character4.setOnAction(e -> {
+			image_room_player_1 = new ImageView("role_4.png");
+			label_room_headpicture1.setGraphic(image_room_player_1);
+			clientCenter.SelectedRole(4);
+		});
+		button_room_character5.setOnAction(e -> {
+			image_room_player_1 = new ImageView("role_5.png");
+			label_room_headpicture1.setGraphic(image_room_player_1);
+			clientCenter.SelectedRole(5);
+		});
+		button_room_ready.setOnAction(e -> {
+			game_init();
+			game_setupUI();
+			game_setupListener(primaryStage);
+			primaryStage.setScene(scene_game);
+
+		});
 	}
-	
+
+	public void game_init() {
+		stackpane_game_root = new StackPane();
+		stackpane_game_backgroundblood = new StackPane();
+		stackpane_game_backgroundground = new StackPane();
+		stackpane_game_backgroundsky = new StackPane();
+		label_game_backgroundblood = new Label();
+		label_game_backgroundsky = new Label();
+		label_game_backgroundground = new Label();
+		label_game_headpicture = new Label();
+		label_game_character1 = new Label();
+		label_game_character2 = new Label();
+		label_game_character3 = new Label();
+		label_game_character4 = new Label();
+		progressbar_game_blood = new ProgressBar(1);
+		progressbar_game_characterblood1 = new ProgressBar(1);
+		progressbar_game_characterblood2 = new ProgressBar(1);
+		progressbar_game_characterblood3 = new ProgressBar(1);
+		progressbar_game_characterblood4 = new ProgressBar(1);
+	}
+
+	public void game_setupUI() {
+
+		label_game_headpicture.setPrefSize(100, 100);
+		label_game_headpicture.setTranslateX(-330);
+		label_game_headpicture.setTranslateY(-220);
+		ImageView imageview_headpicture = new ImageView("img_game_header.png");
+		label_game_headpicture.setGraphic(imageview_headpicture);
+		label_game_backgroundblood.setPrefSize(800, 150);
+		label_game_backgroundblood.setStyle("-fx-background-color: #000000");
+		label_game_backgroundblood.setTranslateX(0);
+		label_game_backgroundblood.setTranslateY(-225);
+
+		label_game_backgroundsky.setPrefSize(800, 200);
+		label_game_backgroundsky.setTranslateX(0);
+		label_game_backgroundsky.setTranslateY(-50);
+		ImageView imageview_backgroundsky = new ImageView("back.jpg");
+		label_game_backgroundsky.setGraphic(imageview_backgroundsky);
+
+		label_game_backgroundground.setPrefSize(800, 250);
+		label_game_backgroundground.setTranslateX(0);
+		label_game_backgroundground.setTranslateY(175);
+		ImageView imageview_backgroundground = new ImageView("ground.jpg");
+		label_game_backgroundground.setGraphic(imageview_backgroundground);
+
+		progressbar_game_blood.setStyle("-fx-accent: red");
+		progressbar_game_blood.setPrefSize(500, 25);
+		progressbar_game_blood.setTranslateX(0);
+		progressbar_game_blood.setTranslateY(-225);
+
+		label_game_character1.setPrefSize(50, 50);
+		label_game_character1.setTranslateX(-350);
+		label_game_character1.setTranslateY(150);
+		ImageView imageview_character1 = new ImageView("img_game_role1.png");
+		label_game_character1.setGraphic(imageview_character1);
+		progressbar_game_characterblood1.setStyle("-fx-accent: red");
+		progressbar_game_characterblood1.setPrefSize(50, 1);
+		progressbar_game_characterblood1.setTranslateX(-350);
+		progressbar_game_characterblood1.setTranslateY(120);
+
+		label_game_character2.setPrefSize(50, 50);
+		label_game_character2.setTranslateX(-350);
+		label_game_character2.setTranslateY(260);
+		ImageView imageview_character2 = new ImageView("img_game_role2.png");
+		label_game_character2.setGraphic(imageview_character2);
+		progressbar_game_characterblood2.setStyle("-fx-accent: red");
+		progressbar_game_characterblood2.setPrefSize(50, 1);
+		progressbar_game_characterblood2.setTranslateX(-350);
+		progressbar_game_characterblood2.setTranslateY(230);
+
+		label_game_character3.setPrefSize(50, 50);
+		label_game_character3.setTranslateX(350);
+		label_game_character3.setTranslateY(150);
+		ImageView imageview_character3 = new ImageView("img_game_role3.png");
+		label_game_character3.setGraphic(imageview_character3);
+		progressbar_game_characterblood3.setStyle("-fx-accent: red");
+		progressbar_game_characterblood3.setPrefSize(50, 1);
+		progressbar_game_characterblood3.setTranslateX(350);
+		progressbar_game_characterblood3.setTranslateY(120);
+
+		label_game_character4.setPrefSize(50, 50);
+		label_game_character4.setTranslateX(350);
+		label_game_character4.setTranslateY(260);
+		ImageView imageview_character4 = new ImageView("img_game_role4.png");
+		label_game_character4.setGraphic(imageview_character4);
+		progressbar_game_characterblood4.setStyle("-fx-accent: red");
+		progressbar_game_characterblood4.setPrefSize(50, 1);
+		progressbar_game_characterblood4.setTranslateX(350);
+		progressbar_game_characterblood4.setTranslateY(230);
+
+		stackpane_game_root.getChildren().addAll(stackpane_game_backgroundblood, stackpane_game_backgroundsky,
+				stackpane_game_backgroundground);
+		stackpane_game_backgroundblood.getChildren().addAll(label_game_backgroundblood, label_game_headpicture,
+				label_game_backgroundsky, label_game_backgroundground);
+		stackpane_game_backgroundblood.getChildren().addAll(progressbar_game_blood);
+		stackpane_game_backgroundground.getChildren().addAll(label_game_character1, progressbar_game_characterblood1);
+		stackpane_game_backgroundground.getChildren().addAll(label_game_character2, progressbar_game_characterblood2);
+		stackpane_game_backgroundground.getChildren().addAll(label_game_character3, progressbar_game_characterblood3);
+		stackpane_game_backgroundground.getChildren().addAll(label_game_character4, progressbar_game_characterblood4);
+		
+		scene_game = new Scene(stackpane_game_root, 800, 600);
+		scene_game.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+	}
+
+	public void game_setupListener(Stage stage) {
+
+	}
+
 	public void start(Stage primaryStage) {
 		login_init();
 		login_setupUI();
@@ -197,12 +319,11 @@ public class Client extends Application {
 		primaryStage.setScene(scene_login);
 		primaryStage.setResizable(false);
 		primaryStage.show();
-		
+
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
 
 }
--- src/application/ClientCenter.java
+++ src/application/ClientCenter.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
 
 public class ClientCenter implements Runnable {
 	private int state;
-	private int Tid;
+	private int Tid , myTid;
 	private String function;
 	private int source;
 	private int dest;
@@ -27,27 +27,28 @@ public class ClientCenter implements Runnable {
 	private PrintStream writer;
 	private String ta[];
 	private Stage primaryStage;
+
 	/*
-	state#Tid#function#source#dest#type#X#Y#direction#SType
-	0      1    2         3     4    5  6 7  8          9
-	private String data[][] = new String[4][10];
-	*/
-	public ClientCenter(Stage stage,Socket socket ,String ip,String name) {
+	 * state#Tid#function#source#dest#type#X#Y#direction#SType 0 1 2 3 4 5 6 7 8
+	 * 9 private String data[][] = new String[4][10];
+	 */
+	public ClientCenter(Stage stage, Socket socket, String ip, String name) {
 		try {
-			EstablishConnection(ip,8888);
+			EstablishConnection(ip, 8888);
 			primaryStage = stage;
-			
+
 		} catch (Exception ex) {
 			System.out.println("連接失敗 in ClientCenter");
 		}
 	}
-	private void EstablishConnection(String ip,int port) {
+
+	private void EstablishConnection(String ip, int port) {
 		try {
 			// 請求建立連線
 			sock = new Socket(ip, port);
 			// 建立I/O資料流
 			InputStreamReader streamReader =
-			// 取得Socket的輸入資料流
+					// 取得Socket的輸入資料流
 					new InputStreamReader(sock.getInputStream());
 			// 放入暫存區
 			reader = new BufferedReader(streamReader);
@@ -61,20 +62,13 @@ public class ClientCenter implements Runnable {
 			System.out.println("建立連線失敗");
 		}
 	}
-/*
-	public class IncomingReader implements Runnable {
-		public void run() {
-			String message;
-			try {
-				while ((message = reader.readLine()) != null) {
-					System.out.print(message + '\n');
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-	}
-*/
+
+	/*
+	 * public class IncomingReader implements Runnable { public void run() {
+	 * String message; try { while ((message = reader.readLine()) != null) {
+	 * System.out.print(message + '\n'); } } catch (Exception ex) {
+	 * ex.printStackTrace(); } } }
+	 */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -113,27 +107,29 @@ public class ClientCenter implements Runnable {
 		Y = Double.parseDouble(ta[7]);
 		direction = Integer.parseInt(ta[8]);
 		Stype = ta[9];
-		System.out.println("state: "+state);
-		System.out.println("Tid: "+Tid);
-		System.out.println("function: "+function);
-		System.out.println("source: "+source);
-		System.out.println("dest: "+dest);
-		System.out.println("type: "+type);
-		System.out.println("X: "+X);
-		System.out.println("Y: "+Y);
-		System.out.println("direction: "+direction);
-		System.out.println("Stype: "+Stype);
+		myTid = Integer.parseInt(ta[5]);
+		System.out.println("state: " + state);
+		System.out.println("Tid: " + Tid);
+		System.out.println("function: " + function);
+		System.out.println("source: " + source);
+		System.out.println("dest: " + dest);
+		System.out.println("type: " + type);
+		System.out.println("X: " + X);
+		System.out.println("Y: " + Y);
+		System.out.println("direction: " + direction);
+		System.out.println("Stype: " + Stype);
+		System.out.println("myTid: " + myTid);
 	}
 
 	public void handle() {
 
 		if (state == 0) {
 			// state 0
-			switch(function){
+			switch (function) {
 			case "connect":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定房間內的角色.名字排序
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定房間內的角色.名字排序
 				case "1":
 					break;
 				case "2":
@@ -144,10 +140,10 @@ public class ClientCenter implements Runnable {
 					break;
 				}
 				break;
-			case "disconnect":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid顯示誰斷線
+			case "connected":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO:
 				case "1":
 					break;
 				case "2":
@@ -158,12 +154,11 @@ public class ClientCenter implements Runnable {
 					break;
 				}
 				break;
-			case "choose":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定房間內誰選擇角色
+			case "disconnected":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid顯示誰斷線
 				case "1":
-					SelectedRole_1();
 					break;
 				case "2":
 					break;
@@ -173,10 +168,70 @@ public class ClientCenter implements Runnable {
 					break;
 				}
 				break;
-			case "lock":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰鎖定角色
+			case "choosed":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定房間內誰選擇角色
+				case "1":
+					// SelectedRole();
+					// Label.set
+					break;
+				case "2":
+					break;
+				case "3":
+					break;
+				case "4":
+					break;
+				}
+				break;
+			case "locked":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰鎖定角色
+				case "1":
+					break;
+				case "2":
+					break;
+				case "3":
+					break;
+				case "4":
+					break;
+				}
+				break;
+			case "go1":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰鎖定角色
+				case "1":
+					break;
+				case "2":
+					break;
+				case "3":
+					break;
+				case "4":
+					break;
+				}
+				break;
+
+			case "go2":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰鎖定角色
+				case "1":
+					break;
+				case "2":
+					break;
+				case "3":
+					break;
+				case "4":
+					break;
+				}
+				break;
+
+			case "initial":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰鎖定角色
 				case "1":
 					break;
 				case "2":
@@ -188,13 +243,13 @@ public class ClientCenter implements Runnable {
 				}
 				break;
 			}
-			
+
 		} else if (state == 1) {
-			switch(function){
+			switch (function) {
 			case "atk2":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰發動攻擊
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰發動攻擊
 				case "1":
 					break;
 				case "2":
@@ -206,9 +261,9 @@ public class ClientCenter implements Runnable {
 				}
 				break;
 			case "atked":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰被攻擊
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰被攻擊
 				case "1":
 					break;
 				case "2":
@@ -220,9 +275,9 @@ public class ClientCenter implements Runnable {
 				}
 				break;
 			case "death":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰死亡
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰死亡
 				case "1":
 					break;
 				case "2":
@@ -233,10 +288,10 @@ public class ClientCenter implements Runnable {
 					break;
 				}
 				break;
-			case "moveup":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰往上
+			case "movedup":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰往上
 				case "1":
 					break;
 				case "2":
@@ -247,10 +302,10 @@ public class ClientCenter implements Runnable {
 					break;
 				}
 				break;
-			case "movedown":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰往下
+			case "moveddown":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰往下
 				case "1":
 					break;
 				case "2":
@@ -261,10 +316,10 @@ public class ClientCenter implements Runnable {
 					break;
 				}
 				break;
-			case "moveleft":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰往左
+			case "movedleft":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰往左
 				case "1":
 					break;
 				case "2":
@@ -275,10 +330,10 @@ public class ClientCenter implements Runnable {
 					break;
 				}
 				break;
-			case "moveright":
-				switch(ta[5]){ 
-				//觸發Tid
-				//TODO: 依照Tid設定誰往右
+			case "movedright":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 依照Tid設定誰往右
 				case "1":
 					break;
 				case "2":
@@ -290,46 +345,61 @@ public class ClientCenter implements Runnable {
 				}
 				break;
 			}
+		} else if(state == 2){
+			switch (function) {
+			case "win":
+				switch (ta[5]) {
+				// 觸發Tid
+				// TODO: 
+				case "1":
+					break;
+				case "2":
+					break;
+				case "3":
+					break;
+				case "4":
+					break;
+				}
+				break;
+				case "back1":
+					switch (ta[5]) {
+					// 觸發Tid
+					// TODO: 
+					case "1":
+						break;
+					case "2":
+						break;
+					case "3":
+						break;
+					case "4":
+						break;
+					}
+					break;
+				case "back2":
+					switch (ta[5]) {
+					// 觸發Tid
+					// TODO: 
+					case "1":
+						break;
+					case "2":
+						break;
+					case "3":
+						break;
+					case "4":
+						break;
+					}
+					break;
+			}
 		}
 	}
-	
-	public void SelectedRole_1(){
-		try{
+
+	public void SelectedRole(int role) {
+		try {
 			function = "choose";
-			writer.println((state+"#"+Tid+"#"+function+"#"+source+"#"+dest+"#"+type+"#"+X+"#"+Y+"#"+direction+"#"+Stype));
+			writer.println((state + "#" + myTid + "#" + function + "#" + source + "#" + dest + "#" + role + "#" + X + "#"
+					+ Y + "#" + direction + "#" + Stype));
 			writer.flush();
-		}catch(Exception ex){
-			System.out.println("送出資料失敗");
-		}
-	}
-	public void SelectedRole_2(){
-		try{
-			writer.println(("0#1#choose#-1#-1#2#-1.0#-1.0#0#@"));
-			writer.flush();
-		}catch(Exception ex){
-			System.out.println("送出資料失敗");
-		}
-	}
-	public void SelectedRole_3(){
-		try{
-			writer.println(("0#1#choose#-1#-1#3#-1.0#-1.0#0#@"));
-			writer.flush();
-		}catch(Exception ex){
-			System.out.println("送出資料失敗");
-		}
-	}public void SelectedRole_4(){
-		try{
-			writer.println(("0#1#choose#-1#-1#4#-1.0#-1.0#0#@"));
-			writer.flush();
-		}catch(Exception ex){
-			System.out.println("送出資料失敗");
-		}
-	}
-	public void SelectedRole_5(){
-		try{
-			writer.println(("0#1#choose#-1#-1#5#-1.0#-1.0#0#@"));
-			writer.flush();
-		}catch(Exception ex){
+		} catch (Exception ex) {
 			System.out.println("送出資料失敗");
 		}
 	}
